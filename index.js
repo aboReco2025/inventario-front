eb576d2627ac96b987077178867a8ef9ba9b92b1
Bryan <brayarenasjaramillo461@gmail.com>
2025-09-18 17:34:21 -0500

    mejorando el css

--- index.js
+++ index.js
@@ -68,3 +68,13 @@ function activarExportar() {
 }
 
 document.addEventListener("DOMContentLoaded", activarExportar);
+
+const menu = document.getElementById("menu");
+const sidebar = document.getElementById("sidebar");
+const main = document.getElementById("main")
+
+menu.addEventListener('click', () => {
+    sidebar.classList.toggle('menu-toggle');
+    menu.classList.toggle('menu-toggle');
+    main.classList.toggle('menu-toggle');
+})
